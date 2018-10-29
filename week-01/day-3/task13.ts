f49d8cb3cdd79b7867eb5ef403687c3878d06e87
Andras Gerencser <andras.gerencser@gmail.com>
2018-10-29 13:30:06 +0100

    This is task13 of day03, week01.

---
+++ week-01/day-3/task13.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+let a: number = 30;
+let b: number = 40;
+let c: number = 60;
+
+console.log('Surface Area: ', 2 * a * b + 2 * b * c + 2 * a * c);
+console.log('Volume: ', a * b * c);
