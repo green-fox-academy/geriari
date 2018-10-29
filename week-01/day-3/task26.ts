b24d85aaad770ab0f73bcd891ef7df6cc2491c6e
Andras Gerencser <andras.gerencser@gmail.com>
2018-10-29 17:42:49 +0100

    This is task26 of day03, week01.

---
+++ week-01/day-3/task26.ts
@@ -0,0 +1,28 @@
+'use strict';
+
+let black: string = "%";
+let white: string = " ";
+let row: string = "";
+
+for (let i: number = 1; i <= 8; i++) {
+    for (let j: number = 1; j <= 4; j++) {
+        if (i % 2 != 0) {
+            row = row + white + black;
+        } else {
+            row = row + black + white;
+        }
+    }
+    console.log(row);
+    row = "";
+}
+// Create a program that draws a chess table like this
+//
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+//
