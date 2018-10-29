17115c54893bb76a5c8043934689d7bd889f2ee3
Andras Gerencser <andras.gerencser@gmail.com>
2018-10-29 12:15:42 +0100

    This is task11 of day03, week01.

---
+++ week-01/day-3/task11.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+let myName: string = 'Gerencsér András';
+let myAge: number = 48;
+let myHeight: number = 1.83;
+let myMarriageStatus: boolean = false;
+
+console.log(myName);
+console.log(myAge);
+console.log(myHeight);
+console.log(myMarriageStatus);
