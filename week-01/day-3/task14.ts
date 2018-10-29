d3b03cb4fdc7abee4121ca1c3c1ec9f966342932
Andras Gerencser <andras.gerencser@gmail.com>
2018-10-29 13:41:35 +0100

    This is task14 of day03, week01.

---
+++ week-01/day-3/task14.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+let currentHours: number = 14;
+let currentMinutes: number = 0;
+let currentSeconds: number = 42;
+
+console.log(60-currentSeconds+(60-currentMinutes-1)*60+(24-currentHours-1)*3600);
