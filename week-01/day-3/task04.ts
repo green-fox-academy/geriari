145d55259519ca1de4fa36707e97a23ec8660dc6
Andras Gerencser <andras.gerencser@gmail.com>
2018-10-29 10:44:33 +0100

    This is task04 of day03, week01.

---
+++ week-01/day-3/task04.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+// Greet 3 of your classmates with this program, in three separate lines like:
+
+console.log('Hello Mesi!');
+console.log('Hello Laci!');
+console.log('Hello Juli!');
