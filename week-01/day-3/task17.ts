1ff1e9a7859239af8f55a4156a92ea590ddf1b1d
Andras Gerencser <andras.gerencser@gmail.com>
2018-10-29 15:06:50 +0100

    This is task17 of day03, week01.

---
+++ week-01/day-3/task17.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+for (let a: number = 0; a <= 500; a++) {
+    if (a % 2 == 0) {
+        console.log(a);
+    }
+}
+// Create a program that prints all the even numbers between 0 and 500
