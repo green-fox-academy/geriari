051ccd989941295d89640a368f7248b213893e37
Andras Gerencser <andras.gerencser@gmail.com>
2018-10-30 10:42:26 +0100

    This is task02 of day04, week01.

---
+++ week-01/day-4/task02.ts
@@ -0,0 +1,16 @@
+export{}
+// -  Create variable named `name` and assign the value `Greenfox` to it
+// -  Create a function called `greet` that greets it's input parameter
+//     -  Greeting is printing e.g. `Greetings, dear Greenfox`
+//     -  Prepare for the special case when no parameters are given
+// -  Greet `name`
+
+let name: string = 'Greenfox';
+
+function greet(a = 'Nobody'): string {
+    return 'Greetings, dear ' + a;
+}
+
+console.log(greet(name));
+
+console.log(greet());
