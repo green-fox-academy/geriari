0a1d571b771268f97714789babd12b03cebddc03
Andras Gerencser <andras.gerencser@gmail.com>
2018-10-30 12:37:03 +0100

    This is task10 of day04, week01.

---
+++ week-01/day-4/task10.ts
@@ -0,0 +1,15 @@
+export{}
+// -  Create a variable named `abc` with the following content: `["Arthur", "Boe", "Chloe"]`
+// -  Swap the first and the third element of `abc`
+
+let abc = ["Arthur", "Boe", "Chloe"];
+
+function swapArrayElements(array: string[], swap1: number, swap2: number) {
+    let tmp = array[swap1];
+    array[swap1] = array[swap2];
+    array[swap2] = tmp;
+}
+
+console.log(abc);
+swapArrayElements(abc, 0, 2);
+console.log(abc);
