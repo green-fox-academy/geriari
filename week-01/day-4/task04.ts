507ff06919bdd7ec2632e33c97f15c55def34017
Andras Gerencser <andras.gerencser@gmail.com>
2018-10-30 11:03:13 +0100

    This is task04 of day04, week01.

---
+++ week-01/day-4/task04.ts
@@ -0,0 +1,12 @@
+export{}
+// -  Write a function called `sum` that sum all the numbers until the given parameter
+// -  The function should return the result
+
+let num: number = 10;
+
+
+function sum(a: number): number {
+    return (1 + a) * a / 2;
+}
+
+console.log(sum(num));
