4d292bf7d5abd49efc0b3920368f93a0a22e9233
Andras Gerencser <andras.gerencser@gmail.com>
2018-10-30 12:09:03 +0100

    This is task08 of day04, week01.

---
+++ week-01/day-4/task08.ts
@@ -0,0 +1,17 @@
+export{}
+// -  Create a variable named `firstList`
+//    with the following content: `[1, 2, 3]`
+// -  Create a variable named `secondList`
+//    with the following content: `[4, 5]`
+// -  Log to the console if `secondList` has more elements than `firstList`
+
+let firstList = [1, 2, 3];
+let secondList = [4, 5];
+
+if (secondList.length < firstList.length) {
+    console.log('fistList has more elements than secondList');
+} else if (secondList.length > firstList.length) {
+    console.log('secondList has more elements than firstList');
+} else {
+    console.log('secondList has as many elements as firstList');
+}
