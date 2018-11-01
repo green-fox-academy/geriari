ace2ee019eaa8f5f3702d59eec05adeac5e7e842
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-01 11:39:08 +0100

    Created this file for my weekly demo.

---
+++ week-01/day-5/demo_primitive_type_example.ts
@@ -0,0 +1,10 @@
+export{}
+
+let name = 'Peter';
+console.log(name); // gives: Peter
+
+let secondName = name;
+console.log(secondName); // gives: Peter
+
+name = 'Pal';
+console.log(secondName); // gives: Peter
