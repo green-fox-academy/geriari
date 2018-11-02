65d697b5a6f8ecf610889ebf113582a86401b0f3
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-02 16:27:08 +0100

    Created the code for the exercise Palindrome builder.

---
+++ week-01/day-5/palindrome_builder.ts
@@ -0,0 +1,11 @@
+export { };
+
+function createPalindrome(text: string): string {
+  let textOriginalLength: number = text.length;
+  for (let i: number = 0; i < textOriginalLength; i++) {
+    text = text + text[textOriginalLength - i - 1];
+  }
+  return text;
+}
+
+console.log(createPalindrome('AB'));
