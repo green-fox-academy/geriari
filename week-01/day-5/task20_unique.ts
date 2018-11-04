27f598ca6320158f11858b2e4361a5f816fb4c47
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-04 09:13:23 +0100

    Cleaned up code.

--- week-01/day-5/task20_unique.ts
+++ week-01/day-5/task20_unique.ts
@@ -1,4 +1,4 @@
-export{}
+export { }
 
 //  Create a function that takes a list of numbers as a parameter
 //  Returns a list of numbers where every number in the list occurs only once
@@ -17,6 +17,5 @@ function unique(arr) {
 }
 
 //  Example
-console.log(unique([1, 11, 34, 11, 52, 61, 1, 34]));
+console.log(unique([1, 11, 34, 11, 52, 34, 61, 1, 34]));
 //  should print: `[1, 11, 34, 52, 61]`
-
