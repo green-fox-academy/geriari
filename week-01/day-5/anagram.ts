904df643b8a2eee26e456474dfa12afee58ceae6
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-05 07:40:05 +0100

    Rewritten the code from scratch.

--- week-01/day-5/anagram.ts
+++ week-01/day-5/anagram.ts
@@ -1,20 +1,19 @@
 export { }
 
 function anagram(text1: string, text2: string): boolean {
-  let res: boolean = true;
-  if (text1.length === text2.length) {
-    for (let i: number = 0; i < text1.length; i++) {
-      if (text1[i] !== text2[text1.length - 1 - i]) {
-        res = false;
-        break;
-      } else {
-        continue;
-      }
+  let arrayText1: string[] = Array.from(text1);
+  let arrayText2: string[] = Array.from(text2);
+  arrayText1.sort();
+  arrayText2.sort();
+  let result: boolean = true;
+  for (let i: number = 0; i < arrayText1.length; i++) {
+    if (arrayText1[i] !== arrayText2[i]) {
+      result = false;
+      break;
+    } else {
+      continue;
     }
-  } else {
-    res = false;
-  }
-  
-  return res;
-} 
-console.log(anagram('mar', 'rim'));
+  } return result;
+}
+
+console.log(anagram('korte', 'kotre'));
