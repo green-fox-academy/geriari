4464eb2a21e6e79bd2a710b6971247efdec7784a
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-06 15:47:17 +0100

    This is exercise: Arrays / 03 Append letter, day 2, week 2.

---
+++ week-02/day-2/03_append_letter.ts
@@ -0,0 +1,16 @@
+'use strict';
+// Create a function called "appendA()" that adds "a" to every string in the far list.
+// The parameter should be a list.
+
+let far: string[] = ['bo', 'anacond', 'koal', 'pand', 'zebr'];
+
+function appendA(list: string[]): string[] {
+  list.forEach((e, i, a) => {a[i] = e + 'a';})
+  return list;
+}
+
+console.log(appendA(far));
+
+// The output should be: 'boa', 'anaconda', 'koala', 'panda', 'zebra'
+
+export = appendA;
