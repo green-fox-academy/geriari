bf0b02266a2f85ec04f05d6230640fbe5ea75a19
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-09 09:05:49 +0100

    This is exercise: 01 Line play, day 5, week 2.

---
+++ week-02/day-5/1_line_play/1_line_play.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+for (let i: number = 0; i < 20; i++) {
+  ctx.beginPath();
+  ctx.moveTo(i * 30, 0);
+  ctx.lineTo(canvas.width, i * 20);
+  ctx.strokeStyle = 'fuchsia';
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.moveTo(0, i * 20);
+  ctx.lineTo(i * 30, canvas.height);
+  ctx.strokeStyle = 'green';
+  ctx.stroke();
+}
