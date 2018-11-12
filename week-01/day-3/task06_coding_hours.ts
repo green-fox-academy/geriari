06ff0a09530694cdaf981a138d2c217b3de877ca
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-12 17:08:02 +0100

    Refactored code.

--- week-01/day-3/task06_coding_hours.ts
+++ week-01/day-3/task06_coding_hours.ts
@@ -1,9 +1,5 @@
 'use strict';
 
-console.log('A Green Fox attendee spends '+6*5*17+' hours with coding in a semester.');
-
-console.log('The percentage of coding hours in average is '+6*5/52*100+' percent.');
-
 // An average Green Fox attendee codes 6 hours daily
 // The semester is 17 weeks long
 //
@@ -12,3 +8,6 @@ console.log('The percentage of coding hours in average is '+6*5/52*100+' percent
 //
 // Print the percentage of the coding hours in the semester if the average
 // work hours weekly is 52
+
+console.log('A Green Fox attendee spends '+6*5*17+' hours with coding in a semester.');
+console.log('The percentage of coding hours in average is '+6*5/52*100+' percent.');
