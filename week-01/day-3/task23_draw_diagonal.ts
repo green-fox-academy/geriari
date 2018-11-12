64888ac62695d48d7136e9af29b98b0896a3ba06
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-12 17:45:15 +0100

    Refactored code.

--- week-01/day-3/task23_draw_diagonal.ts
+++ week-01/day-3/task23_draw_diagonal.ts
@@ -1,30 +1,5 @@
 'use strict';
 
-let lineCount: number = 5;
-
-let topAndBottom: string = "";
-let middle: string = "";
-
-//initialize topAndBottom
-for (let i: number = 1; i <= lineCount; i++) {
-    topAndBottom = topAndBottom + "%";
-}
-
-//print to console
-console.log(topAndBottom);
-for (let j: number = 1; j <= lineCount -2; j++) {
-    for (let k: number = 1; k <= lineCount; k++) {
-        if (k == 1 || k == lineCount || k == j + 1) {
-            middle = middle + "%";
-        } else {
-            middle = middle + " ";
-        }
-    }
-    console.log(middle);
-    middle = "";
-}
-console.log(topAndBottom);
-
 // Write a program that draws a
 // square like this:
 //
@@ -36,3 +11,26 @@ console.log(topAndBottom);
 // %%%%%
 //
 // The square should have as many lines as lineCount is
+
+let lineCount: number = 5;
+
+let topAndBottom: string = '';
+let middle: string = '';
+
+for (let i: number = 1; i <= lineCount; i++) {
+  topAndBottom = topAndBottom + '%';
+}
+
+console.log(topAndBottom);
+for (let j: number = 1; j <= lineCount -2; j++) {
+  for (let k: number = 1; k <= lineCount; k++) {
+    if (k === 1 || k === lineCount || k === j + 1) {
+      middle = middle + '%';
+      } else {
+      middle = middle + ' ';
+      }
+    }
+    console.log(middle);
+    middle = '';
+}
+console.log(topAndBottom);
