e4e951d8cb390bbc3394fc52817615b0826baddf
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-12 17:23:26 +0100

    Refactored code.

--- week-01/day-3/task18_fizz_buzz.ts
+++ week-01/day-3/task18_fizz_buzz.ts
@@ -1,17 +1,18 @@
 'use strict';
 
-for (let a: number = 1; a <= 100; a++) {
-    if (a % 3 != 0 && a % 5 != 0) {
-        console.log(a);
-    } else if (a % 3 == 0 && a % 5 == 0) {
-        console.log("FizzBuzz");
-    } else if (a % 3 == 0) {
-        console.log("Fizz");
-    } else {
-        console.log("Buzz");
-    }
-}
 // Write a program that prints the numbers from 1 to 100.
 // But for multiples of three print “Fizz” instead of the number
 // and for the multiples of five print “Buzz”.
 // For numbers which are multiples of both three and five print “FizzBuzz”.
+
+for (let a: number = 1; a <= 100; a++) {
+  if (a % 3 != 0 && a % 5 != 0) {
+    console.log(a);
+  } else if (a % 3 == 0 && a % 5 == 0) {
+    console.log('FizzBuzz');
+  } else if (a % 3 == 0) {
+    console.log('Fizz');
+  } else {
+    console.log('Buzz');
+  }
+}
