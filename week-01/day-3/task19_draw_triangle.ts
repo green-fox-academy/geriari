5bcc9c38842b62bb75a631a7acbe68f1603f05d7
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-12 17:24:38 +0100

    Refactored code.

--- week-01/day-3/task19_draw_triangle.ts
+++ week-01/day-3/task19_draw_triangle.ts
@@ -1,14 +1,5 @@
 'use strict';
-
-let lineCount: number = 4;
-let currentStar: string = "";
-let oneStar: string = "*";
-
-for (let i: number = 1; i <= lineCount; i++) {
-    currentStar = currentStar + oneStar;
-    console.log(currentStar);
-}
-
+export { };
 
 // Write a program that draws a triangle like this:
 //
@@ -18,3 +9,12 @@ for (let i: number = 1; i <= lineCount; i++) {
 // ****
 //
 // The triangle should have as many lines as lineCount is
+
+let lineCount: number = 4;
+let currentStar: string = '';
+let oneStar: string = '*';
+
+for (let i: number = 1; i <= lineCount; i++) {
+  currentStar = currentStar + oneStar;
+  console.log(currentStar);
+}
