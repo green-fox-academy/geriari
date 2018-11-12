22e9cf2625fbf505b80d7be1b63f8bbd0a76097f
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-12 17:30:19 +0100

    Refactored code.

--- week-01/day-3/task20_draw_pyramid.ts
+++ week-01/day-3/task20_draw_pyramid.ts
@@ -1,23 +1,6 @@
 'use strict';
+export { };
 
-let lineCount: number = 4;
-let oneSpace: string = " ";
-let twoStars: string = "**";
-let currentSpace: string = "";
-let currentStar: string = "*";
-let currentSpaceNumber: number = lineCount - 1;
-let currentStarNumber: number = 1;
-
-//initialize currentSpaceNumber
-for (let i: number = 1; i <= currentSpaceNumber; i++) {
-    currentSpace = currentSpace + " ";
-}
-
-for (let j: number = 1; j <= lineCount; j++) {
-    console.log(currentSpace + currentStar);
-    currentStar = currentStar + twoStars;
-    currentSpace = currentSpace.slice(0, -1);
-}
 // Write a program that draws a
 // pyramid like this:
 //
@@ -27,3 +10,17 @@ for (let j: number = 1; j <= lineCount; j++) {
 // *******
 //
 // The pyramid should have as many lines as lineCount is
+
+let lineCount: number = 4;
+let currentSpace: string = '';
+let currentStar: string = '*';
+
+for (let i: number = 1; i < lineCount; i++) {
+  currentSpace = currentSpace + " ";
+}
+
+for (let j: number = 1; j <= lineCount; j++) {
+  console.log(currentSpace + currentStar);
+  currentStar = currentStar + '**';
+  currentSpace = currentSpace.slice(0, -1);
+}
