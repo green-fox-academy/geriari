c043e1751b08839ba4b099c3deed33136214c74e
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-12 17:16:48 +0100

    Refactored code.

--- week-01/day-3/task13_seconds_in_a_day.ts
+++ week-01/day-3/task13_seconds_in_a_day.ts
@@ -4,4 +4,4 @@ let currentHours: number = 14;
 let currentMinutes: number = 0;
 let currentSeconds: number = 42;
 
-console.log(60-currentSeconds+(60-currentMinutes-1)*60+(24-currentHours-1)*3600);
+console.log(60 - currentSeconds + (60 - currentMinutes - 1) * 60 + (24 - currentHours - 1) * 3600);
