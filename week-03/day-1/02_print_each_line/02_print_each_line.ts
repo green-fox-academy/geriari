ae88510debb8d3d8eb184311c9d1c0888dfef2c3
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-12 12:28:50 +0100

    This is exercise 02 Print each line, day 1, week 3.

---
+++ week-03/day-1/02_print_each_line/02_print_each_line.ts
@@ -0,0 +1,26 @@
+import { fileURLToPath } from "url";
+
+// Write a program that opens a file called 'my-file.txt', then prints
+// each of lines form the file.
+// If the program is unable to read the file (for example it does not exists),
+// then it should print an error message like: 'Unable to read file: my-file.txt'
+
+export { };
+const fs = require('fs');
+
+function readFromFile(fileName: string): string {
+  try {
+    return fs.readFileSync(fileName, 'utf-8');
+  } catch(e) {
+    return null;
+  }
+}
+
+const fileContent: string = readFromFile('my-file.txt');
+if (fileContent !== null) {
+  fileContent.split('\n').forEach(e => {
+    console.log(e);
+  });  
+} else {
+  console.log('Unable to read file: my-file.txt');
+}
