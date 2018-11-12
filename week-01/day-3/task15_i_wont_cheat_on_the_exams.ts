fb157a45756779de464d1e662f138f333d7a678f
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-12 17:19:42 +0100

    Refactored code.

--- week-01/day-3/task15_i_wont_cheat_on_the_exams.ts
+++ week-01/day-3/task15_i_wont_cheat_on_the_exams.ts
@@ -1,8 +1,9 @@
 'use strict';
 
-for (let a: number = 0; a < 100; a++) {
-    console.log("I won't cheat the exam!");
-  }
-
 // Create a program that writes this line 100 times:
 // "I won't cheat on the exam!"
+
+for (let a: number = 0; a < 100; a++) {
+    console.log('I won\'t cheat the exam!');
+  }
+
