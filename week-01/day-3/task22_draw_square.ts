46edb38aab6b77f98bd7a108b00d9e52890df88e
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-12 17:41:38 +0100

    Refactored code.

--- week-01/day-3/task22_draw_square.ts
+++ week-01/day-3/task22_draw_square.ts
@@ -1,28 +1,5 @@
 'use strict';
-
-let lineCount: number = 6;
-
-let topAndBottom: string = "";
-let middle: string = "";
-
-//initialize topAndBottom
-for (let i: number = 1; i <= lineCount; i++) {
-    topAndBottom = topAndBottom + "%";
-}
-
-//initialize middle
-middle = "%";
-for (let j: number = 1; j <= lineCount -2; j++) {
-    middle = middle + " ";
-}
-middle = middle + "%";
-
-//print to console
-console.log(topAndBottom);
-for (let k: number = 1; k <= lineCount -2; k++) {
-    console.log(middle);
-}
-console.log(topAndBottom);
+export { };
 
 // Write a program that draws a square like this:
 //
@@ -34,3 +11,24 @@ console.log(topAndBottom);
 // %%%%%%
 //
 // The square should have as many lines as lineCount is
+
+let lineCount: number = 6;
+
+let topAndBottom: string = '';
+let middle: string = '';
+
+for (let i: number = 1; i <= lineCount; i++) {
+  topAndBottom = topAndBottom + "%";
+}
+
+middle = '%';
+for (let j: number = 1; j <= lineCount -2; j++) {
+  middle = middle + ' ';
+}
+middle = middle + '%';
+
+console.log(topAndBottom);
+for (let k: number = 1; k <= lineCount -2; k++) {
+  console.log(middle);
+}
+console.log(topAndBottom);
