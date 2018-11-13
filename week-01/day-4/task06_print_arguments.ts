196facf4e09a6efc4168db2175cad942481caa9a
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-13 17:29:08 +0100

    Refactored code.

--- week-01/day-4/task06_print_arguments.ts
+++ week-01/day-4/task06_print_arguments.ts
@@ -1,12 +1,13 @@
 export{}
+
 // -  Create a function called `printParams`
 //    which logs to the console the input parameters
 //    (can have multiple number of arguments)
 
 function printParams(...arg: any[]) {
-    for (let i = 0; i < arg.length; i++) {
-        console.log(arg[i]);
-    }
+  for (let i = 0; i < arg.length; i++) {
+    console.log(arg[i]);
+  }
 }
 
 printParams(1, 'alma', true, 2.35);
