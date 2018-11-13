8cff9734f911509596f20363b66235f6d10ccd18
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-13 17:31:31 +0100

    Refactored code.

--- week-01/day-4/task08_compare_length.ts
+++ week-01/day-4/task08_compare_length.ts
@@ -1,4 +1,5 @@
 export{}
+
 // -  Create a variable named `firstList`
 //    with the following content: `[1, 2, 3]`
 // -  Create a variable named `secondList`
@@ -9,9 +10,9 @@ let firstList = [1, 2, 3];
 let secondList = [4, 5];
 
 if (secondList.length < firstList.length) {
-    console.log('fistList has more elements than secondList');
+  console.log('firstList has more elements than secondList');
 } else if (secondList.length > firstList.length) {
-    console.log('secondList has more elements than firstList');
+  console.log('secondList has more elements than firstList');
 } else {
-    console.log('secondList has as many elements as firstList');
+  console.log('secondList has as many elements as firstList');
 }
