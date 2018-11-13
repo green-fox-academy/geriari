ac47be32ea732d1fb7dc75119efc98e4a5edfc65
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-13 17:13:55 +0100

    Refactored code.

--- week-01/day-3/task16_print_even.ts
+++ week-01/day-3/task16_print_even.ts
@@ -3,7 +3,7 @@
 // Create a program that prints all the even numbers between 0 and 500
 
 for (let a: number = 0; a <= 500; a++) {
-    if (a % 2 == 0) {
-        console.log(a);
-    }
+  if (a % 2 == 0) {
+    console.log(a);
+  }
 }
