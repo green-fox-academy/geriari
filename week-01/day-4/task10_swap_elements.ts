c9a5791d6b1e61397f7407f2f8da7d26de9e88dd
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-13 17:35:18 +0100

    Refactored code.

--- week-01/day-4/task10_swap_elements.ts
+++ week-01/day-4/task10_swap_elements.ts
@@ -1,13 +1,14 @@
 export{}
+
 // -  Create a variable named `abc` with the following content: `["Arthur", "Boe", "Chloe"]`
 // -  Swap the first and the third element of `abc`
 
 let abc = ["Arthur", "Boe", "Chloe"];
 
 function swapArrayElements(array: string[], swap1: number, swap2: number) {
-    let tmp = array[swap1];
-    array[swap1] = array[swap2];
-    array[swap2] = tmp;
+  let tmp = array[swap1];
+  array[swap1] = array[swap2];
+  array[swap2] = tmp;
 }
 
 console.log(abc);
