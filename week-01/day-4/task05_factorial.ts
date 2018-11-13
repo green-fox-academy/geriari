0e6029ae9233251be33083a0297b859c736e84fa
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-13 17:28:22 +0100

    Refactored code.

--- week-01/day-4/task05_factorial.ts
+++ week-01/day-4/task05_factorial.ts
@@ -1,15 +1,16 @@
 export{}
+
 // -  Create a function called `factorio`
 //    that returns it's input's factorial
 
 let num: number = 5;
 
 function factorio(a: number): number {
-    let f: number = 1;
-    for (let i: number = 2; i <= a; i++) {
-        f = f * i;
-    } 
-    return f; 
+  let f: number = 1;
+  for (let i: number = 2; i <= a; i++) {
+    f = f * i;
+  } 
+  return f; 
 }
 
 console.log(factorio(num));
