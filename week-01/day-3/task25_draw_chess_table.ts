39ddb0d79494791b6cb73e9c3dfcf152ff5c5689
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-13 17:18:06 +0100

    Refactored code.

--- week-01/day-3/task25_draw_chess_table.ts
+++ week-01/day-3/task25_draw_chess_table.ts
@@ -1,20 +1,5 @@
 'use strict';
 
-let black: string = "%";
-let white: string = " ";
-let row: string = "";
-
-for (let i: number = 1; i <= 8; i++) {
-    for (let j: number = 1; j <= 4; j++) {
-        if (i % 2 != 0) {
-            row = row + white + black;
-        } else {
-            row = row + black + white;
-        }
-    }
-    console.log(row);
-    row = "";
-}
 // Create a program that draws a chess table like this
 //
 // % % % %
@@ -26,3 +11,19 @@ for (let i: number = 1; i <= 8; i++) {
 // % % % %
 //  % % % %
 //
+
+let black: string = '%';
+let white: string = ' ';
+let row: string = '';
+
+for (let i: number = 1; i <= 8; i++) {
+  for (let j: number = 1; j <= 4; j++) {
+    if (i % 2 !== 0) {
+      row = row + white + black;
+    } else {
+      row = row + black + white;
+    }
+  }
+  console.log(row);
+  row = '';
+}
