0d4458f834d07e2b88b4463cdd0b4ecd8c85a5d9
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-13 11:29:09 +0100

    This is exercise 05 Counter, day 2, week 3.

---
+++ week-03/day-2/05_counter.ts
@@ -0,0 +1,37 @@
+// Create a Counter class
+// which has an integer property
+// when creating it should have a default value 0 or we can specify it when creating
+// we can add(number) to increase the counter's value by a whole number
+// or we can add() without parameters just increasing the counter's value by one
+// and we can get() the current value as string
+// also we can reset() the value to the initial value
+
+class Counter {
+  count: number;
+
+  constructor(baseCount: number = 0) {
+    this.count = Math.round(baseCount);
+  }
+
+  add(increment: number = 1) {
+    this.count += Math.round(increment);
+  }
+
+  get() {
+    return String(this.count);
+  }
+
+  reset() {
+    this.count = 0;
+  }
+}
+
+let num = new Counter(100);
+console.log(num);
+num.add();
+console.log(num);
+num.add(13);
+console.log(num);
+console.log(num.get());
+num.reset();
+console.log(num);
