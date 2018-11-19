242735feed259811b4057a3af770df23f9645c1a
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-19 17:36:13 +0100

    feat(Aircraft Carrier) added Carrier class.

---
+++ week-04/day-1/03_aircraft_carrier/Carrier.ts
@@ -0,0 +1,39 @@
+import { Aircraft } from "./Aircraft";
+
+export class Carrier {
+  protected name: string;
+  protected aircrafts: Aircraft[] = [];
+  protected ammoStore: number;
+  protected healthPoint: number;
+
+  constructor(name: string, ammoStore: number, healthPoint: number) {
+    this.name = name;
+    this.ammoStore = ammoStore;
+    this.healthPoint = healthPoint;
+  }
+
+  addAircraft(aircraft: Aircraft): void {
+    this.aircrafts.push(aircraft);
+  }
+
+  fill(): void {
+    if (this.ammoStore === 0) {
+      //
+    }
+    let sumAmmoNeeded: number = 0;
+    this.aircrafts.forEach((e) => {
+      sumAmmoNeeded += e.getAmmoNeeded(); 
+    });
+    if (sumAmmoNeeded <= this.ammoStore) {
+      this.aircrafts.forEach((e) => {
+        e.refill(this.ammoStore);
+      });
+    } else {
+      this.aircrafts.forEach((e) => {
+        if (e.isPriority) {
+          e.refill(this.ammoStore);
+        }
+      });
+    }
+  }
+}
