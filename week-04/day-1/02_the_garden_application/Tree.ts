af59844c7c2f7bd93023f3ec2b9e92b0d326a32f
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-19 13:37:43 +0100

    feat(Garden Application) added Tree class.

---
+++ week-04/day-1/02_the_garden_application/Tree.ts
@@ -0,0 +1,15 @@
+import { Plant } from './Plant';
+
+export class Tree extends Plant {
+  constructor(color: string, waterLevel: number) {
+    super(color, waterLevel);
+  }
+
+  needsWater(): boolean {
+    return this.waterLevel < 10;
+  }
+
+  water(water: number): void {
+    this.waterLevel+= 0.4 * water;
+  }
+}
