75039ce95cd98f15c428370fdfe93b2abee39e78
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-21 16:30:09 +0100

    refact(Append a).

--- week-01/day-4/task14_append_a.ts
+++ week-01/day-4/task14_append_a.ts
@@ -1,12 +1,13 @@
-export{}
+export { }
+
 // -  Create an array variable named `animals`
 //    with the following content: `["koal", "pand", "zebr"]`
 // -  Add all elements an `"a"` at the end
 
-let animals = ["koal", "pand", "zebr"];
+let animals: string[] = ["koal", "pand", "zebr"];
 
 animals.forEach((element, index, array) => {
-    array[index] = element + 'a';
+  array[index] = element + 'a';
 });
 
 console.log(animals);
