c31d69a3415509aa3350dbc5b14e6a127b3a2d88
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-21 16:37:50 +0100

    refact(Sum all elements).

--- week-01/day-4/task17_sum_all_elements.ts
+++ week-01/day-4/task17_sum_all_elements.ts
@@ -1,13 +1,13 @@
-export{}
+export { }
+
 // -  Create a variable named `ai` with the following content: `[3, 4, 5, 6, 7]`
 // -  Log the sum of the elements in `ai` to the console
 
-let ai = [3, 4, 5, 6, 7];
-
-let sum = 0;
+let ai: number[] = [3, 4, 5, 6, 7];
+let sum: number = 0;
 
 ai.forEach((element) => {
-    sum = sum + element;
+  sum += element;
 });
 
 console.log(sum);
