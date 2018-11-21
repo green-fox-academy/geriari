f6ae634be8c4df6eefd9a0bcfb649b0af0e1536b
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-21 14:11:57 +0100

    feat(Flyable) added constructor to the class.

--- week-04/day-3/05_flyable/helicopter.ts
+++ week-04/day-3/05_flyable/helicopter.ts
@@ -2,6 +2,13 @@ import { Vehicle } from "./vehicle";
 import { Flyable } from "./flyable";
 
 export class Helicopter extends Vehicle implements Flyable  {
+  constructor(property1: string, property2: number, property3: number) {
+    super();
+    super.field1 = property1;
+    super.field2 = property2;
+    super.field3 = property3;
+  }
+  
   land() {
     console.log('Hello, szia!');
   }
