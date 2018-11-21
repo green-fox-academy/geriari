35fea38f3ab266f5ee1bb517069c576404b2e889
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-21 16:27:41 +0100

    refact(Change element).

--- week-01/day-4/task12_change_element.ts
+++ week-01/day-4/task12_change_element.ts
@@ -1,16 +1,17 @@
-export{}
+export { }
+
 // -  Create an array named `numList` with the following content: `[1, 2, 3, 8, 5, 6]`
 // -  Change the 8 to 4 with the `.map` method 
 // -  Print the fourth element as a test
 
-let numList = [1, 2, 3, 8, 5, 6];
+let numList: number[] = [1, 2, 3, 8, 5, 6];
 
 let numList2 = numList.map((element) => {
-    if (element == 8) {
-        return 4;
-    } else {
-        return element;
-    }
+  if (element === 8) {
+    return 4;
+  } else {
+    return element;
+  }
 });
 
 console.log(numList2[3]);
