4a3182bc683320d9dcc7f4a890ae6cb3168c5f4f
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-21 16:28:46 +0100

    refact(Increment element).

--- week-01/day-4/task13_increment_element.ts
+++ week-01/day-4/task13_increment_element.ts
@@ -1,11 +1,11 @@
-export{}
+export { }
+
 // -  Create a variable named `numList` with the following content: `[1, 2, 3, 4, 5]`
 // -  Increment the third element simply by accessing it
 // -  Log the third element to the console
 
-let numList = [1, 2, 3, 4, 5];
+let numList: number[] = [1, 2, 3, 4, 5];
 
-numList[2]++;
+numList[2] ++;
 
 console.log(numList[2]);
-
