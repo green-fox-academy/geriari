1a09123a63ca744d6d9dd581cfc6f39af8bb0980
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-22 14:05:56 +0100

    feat(Fibonacci) finished test.

--- week-04/day-4/05_fibonacci/test.ts
+++ week-04/day-4/05_fibonacci/test.ts
@@ -1,7 +1,27 @@
 import { test } from 'tape';
 import { fibonacci } from './function';
 
+test('Fibonacci function, parameter: 0', t => {
+  t.deepEqual(fibonacci(0), 0);
+  t.end();
+});
+
+test('Fibonacci function, parameter: 1', t => {
+  t.deepEqual(fibonacci(1), 1);
+  t.end();
+});
+
+test('Fibonacci function, parameter: 2', t => {
+  t.deepEqual(fibonacci(2), 1);
+  t.end();
+});
+
 test('Fibonacci function, parameter: 4', t => {
   t.deepEqual(fibonacci(4), 3);
+  t.end();
+});
+
+test('Fibonacci function, parameter: 10', t => {
+  t.deepEqual(fibonacci(10), 55);
   t.end();
 });
