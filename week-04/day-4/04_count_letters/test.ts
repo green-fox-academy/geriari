b982daf9848838f6b23d98592e8fce0e9044bbf7
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-22 13:50:25 +0100

    feat(Count letters) finished test.

--- week-04/day-4/04_count_letters/test.ts
+++ week-04/day-4/04_count_letters/test.ts
@@ -1,13 +1,17 @@
 import { test } from 'tape';
 import { countLetters } from './function';
 
-test('Count letter function test with a normal string', t => {
-  t.equal(countLetters('zfdggaaa'), {
-    a: 3,
-    d: 1,
-    f: 1,
-    g: 2,
-    z: 1
-  });
+test('Count letter function test with a string with lowercase letters', t => {
+  t.deepEqual(countLetters('zfdggaaa'), {a: 3, d: 1, f: 1, g: 2, z: 1});
+  t.end();
+});
+
+test('Count letter function test with a string with lowercase and uppercase letters', t => {
+  t.deepEqual(countLetters('zFdggAaa'), {A: 1, F: 1, a: 2, d: 1, g: 2, z: 1});
+  t.end();
+});
+
+test('Count letter function test with an empty string', t => {
+  t.deepEqual(countLetters(''), {});
   t.end();
 });
