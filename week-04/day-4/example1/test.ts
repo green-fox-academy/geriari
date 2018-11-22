b601f3c702dc8de7f25b5b91763ac30e331af7fe
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-22 11:58:27 +0100

    feat(Example1) created test.

---
+++ week-04/day-4/example1/test.ts
@@ -0,0 +1,15 @@
+import { test } from 'tape';
+import { addNum, subNum } from './function';
+
+test('add 2 numbers', t => {
+  const actual = addNum(1, 2);
+  const expected = 3;
+
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('test subNum()', t => {
+  t.equal(subNum(2, 1), 1);
+  t.end();
+})
