b61f578e45fa88cc8b97a8ddd2f9a03d71b4d23b
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-22 14:29:43 +0100

    feat(Fibonacci) added exception to function.

--- week-04/day-4/05_fibonacci/function.ts
+++ week-04/day-4/05_fibonacci/function.ts
@@ -1,8 +1,11 @@
 export function fibonacci(n: number): number {
-  if (n === 0) {
+  if (n < 0) {
+    throw('Error: negative number.');
+  } else if (n === 0) {
     return 0;
   } else if (n === 1) {
     return 1;
-  }
+  } else {
   return fibonacci(n - 2) + fibonacci(n - 1);
+  } 
 }
