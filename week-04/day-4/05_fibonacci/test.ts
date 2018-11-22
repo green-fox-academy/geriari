c32aaa67bf5dfe3fc323c99fbddff6fd7bb86de7
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-22 14:29:14 +0100

    feat(Fibonacci) added test with parameter -1.

--- week-04/day-4/05_fibonacci/test.ts
+++ week-04/day-4/05_fibonacci/test.ts
@@ -2,26 +2,31 @@ import { test } from 'tape';
 import { fibonacci } from './function';
 
 test('Fibonacci function, parameter: 0', t => {
-  t.deepEqual(fibonacci(0), 0);
+  t.equal(fibonacci(0), 0);
   t.end();
 });
 
 test('Fibonacci function, parameter: 1', t => {
-  t.deepEqual(fibonacci(1), 1);
+  t.equal(fibonacci(1), 1);
   t.end();
 });
 
 test('Fibonacci function, parameter: 2', t => {
-  t.deepEqual(fibonacci(2), 1);
+  t.equal(fibonacci(2), 1);
   t.end();
 });
 
 test('Fibonacci function, parameter: 4', t => {
-  t.deepEqual(fibonacci(4), 3);
+  t.equal(fibonacci(4), 3);
   t.end();
 });
 
 test('Fibonacci function, parameter: 10', t => {
-  t.deepEqual(fibonacci(10), 55);
+  t.equal(fibonacci(10), 55);
+  t.end();
+});
+
+test('Fibonacci function, parameter: -1', t => {
+  t.throws(fibonacci(-1), 'Error: negative number.');
   t.end();
 });
