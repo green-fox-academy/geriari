30b843f8ccda1933ff7ee27a12f74e9b67eb5220
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-22 15:00:58 +0100

    feat(Fibonacci) added exception for floating numbers to function.

--- week-04/day-4/05_fibonacci/function.ts
+++ week-04/day-4/05_fibonacci/function.ts
@@ -1,6 +1,8 @@
 export function fibonacci(n: number): number {
   if (n < 0) {
-    throw('Error: negative number.');
+    throw new Error('Error: negative number.');
+  } else if (n % 1 !== 0) {
+    throw new Error('Error: not an integer number.');
   } else if (n === 0) {
     return 0;
   } else if (n === 1) {
