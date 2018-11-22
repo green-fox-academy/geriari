b8564fe061d6338bc3586a03c392c81805723cda
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-22 13:35:03 +0100

    feat(Count letters) created function.

---
+++ week-04/day-4/04_count_letters/function.ts
@@ -0,0 +1,31 @@
+// let betu = 'A';
+// let betuSzam = 5;
+
+// let alma = {
+//   [betu]: betuSzam,
+// }
+
+// console.log(alma.A);
+
+
+
+// if ('key' in myObj) - csekkolja, hogy létezik-e az objektumban egy key
+
+// var obj = {key1: value1, key2: value2};
+// obj.key3 = "value3"; // egy meglévő objektumba új keyt tesz bele értékkel együtt
+
+export function countLetters(text: string): Object {
+  let textArray: string[] = text.split('').sort();
+  let dictionary: Object = {};
+  for (let i: number = 0; i < textArray.length; i++) {
+    if (textArray[i] in dictionary) {
+      dictionary[textArray[i]] ++;
+    } else {
+      dictionary[textArray[i]] = 1;
+    }
+  }
+  return dictionary;
+} 
+
+console.log(countLetters('hhhgtfdaa'));
+
