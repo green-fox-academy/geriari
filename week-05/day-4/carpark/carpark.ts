afa33bb4273743bdb3aa552ed012619d8201368c
Andras Gerencser <andras.gerencser@gmail.com>
2018-11-29 14:40:34 +0100

    feat(Carpark) add getOldest() getPenalty() methods to CarPark class.

--- week-05/day-4/carpark/carpark.ts
+++ week-05/day-4/carpark/carpark.ts
@@ -5,15 +5,15 @@ import { writeFile } from 'fs';
 export class CarPark {
   cars: Car[] = [];
 
-  addCar(car: Car) {
+  addCar(car: Car): void {
     this.cars.push(car);
   }
 
-  addCarToFile(car: Car, fileName: string) {
+  addCarToFile(car: Car, fileName: string): void {
     appendToFile(fileName, `\n${car.getLicensePlate()},${car.getManufactureYear()},${car.getHasParkingTicket()}`);
   }
 
-  removeCar(licensePlate: string, fileName: string) {
+  removeCar(licensePlate: string, fileName: string): void {
     this.cars.forEach((e, i) => {
       if (e.getLicensePlate() === licensePlate) {
         this.cars.splice(i, 1);
@@ -34,6 +34,33 @@ export class CarPark {
         tempString += e[0] + ',' + e[1] + ',' + e[2] + '\n';
       });
       writeToFile(fileName, tempString);
+    }
+  }
+
+  getOldest(fileName: string): string {
+    let fileContent = readFromFile(fileName);
+    if (fileContent !== null) {
+      let tempArray: string[] = fileContent.split('\n');
+      let carArray: Car[] = tempArray.map(line => {
+        return new Car(line.split(',')[0], parseInt(line.split(',')[1]), parseInt(line.split(',')[2]));
+      });
+      carArray.sort((a, b) => {
+        return a.getManufactureYear() - b.getManufactureYear();
+      });
+      return carArray[0].getLicensePlate();
+    }
+  }
+
+  getPenalty(fileName: string): Car[] {
+    let fileContent = readFromFile(fileName);
+    if (fileContent !== null) {
+      let tempArray: string[] = fileContent.split('\n');
+      let carArray: Car[] = tempArray.map(line => {
+        return new Car(line.split(',')[0], parseInt(line.split(',')[1]), parseInt(line.split(',')[2]));
+      });
+      return carArray.filter(e => {
+        e.getHasParkingTicket() === 0;
+      });
     }
   }
 }
