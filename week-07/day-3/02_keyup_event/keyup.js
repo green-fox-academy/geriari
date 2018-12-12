9229eb4a84187727afdf47ebafcbe9df21e8be91
Andras Gerencser <andras.gerencser@gmail.com>
2018-12-12 11:39:24 +0100

    feat(Keyup event) add js

---
+++ week-07/day-3/02_keyup_event/keyup.js
@@ -0,0 +1,9 @@
+// 1) Subscibe to keyup events on the global window object
+// 2) Console log the event object and peek inside
+// 3) Display the last pressed key's code as "Last pressed key code is: __"
+
+window.addEventListener('keyup', (e) => {
+  //console.log(e.keyCode);
+  const text = document.querySelector('h1');
+  text.innerText = `Last pressed key code is: ${e.keyCode}`;
+});
