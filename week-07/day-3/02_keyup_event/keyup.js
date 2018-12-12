92f515062f59bcb7abac5d3bccc1f2b569997910
Andras Gerencser <andras.gerencser@gmail.com>
2018-12-12 11:40:04 +0100

    refact(Keyup event) add js

--- week-07/day-3/02_keyup_event/keyup.js
+++ week-07/day-3/02_keyup_event/keyup.js
@@ -3,7 +3,6 @@
 // 3) Display the last pressed key's code as "Last pressed key code is: __"
 
 window.addEventListener('keyup', (e) => {
-  //console.log(e.keyCode);
   const text = document.querySelector('h1');
   text.innerText = `Last pressed key code is: ${e.keyCode}`;
 });
