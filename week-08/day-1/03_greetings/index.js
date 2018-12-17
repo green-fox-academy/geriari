5ac2525b53cd0ec9e9093179f39de6886a0210b2
Andras Gerencser <andras.gerencser@gmail.com>
2018-12-17 14:28:19 +0100

    feat(Greetings) add js

---
+++ week-08/day-1/03_greetings/index.js
@@ -0,0 +1,15 @@
+let express = require('express');
+let app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    title: `Welcome back ${req.query.name || 'Guest'}!`,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
+});
