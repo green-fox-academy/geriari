00035c83226428b49bd72ef1b8ffcb8c846d3a5b
Andras Gerencser <andras.gerencser@gmail.com>
2018-12-21 14:47:09 +0100

    feat(Bookstore) add filtering functionality to backend js

--- week-08/day-4/01_bookstore_endpoints/app.js
+++ week-08/day-4/01_bookstore_endpoints/app.js
@@ -9,7 +9,6 @@ const PORT = 3000;
 app.use(express.json());
 app.use('/static', express.static('static'));
 
-// CONFIGURATION
 const conn = mysql.createConnection({
   port: 3306,
   user: 'root',
@@ -21,22 +20,21 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/books', (req, res) => {
-  const sqlQuery = 'SELECT book_name FROM book_mast;';
-  conn.query(sqlQuery, (err, data) => {
-    if (err) {
-      console.log(err.message);
-      res.status(500).json({
-        error: 'Internal server error'
-      });
-      return;
-    }
-    res.json(data);
-  });
-});
-
 app.get('/booksfulldata', (req, res) => {
-  const sqlQuery = 'SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast, author, category, publisher WHERE book_mast.aut_id = author.aut_id AND book_mast.cate_id = category.cate_id AND book_mast.pub_id = publisher.pub_id;';
+  let sqlQuery = 'SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast, author, category, publisher WHERE book_mast.aut_id = author.aut_id AND book_mast.cate_id = category.cate_id AND book_mast.pub_id = publisher.pub_id';
+  if (req.query.category) {
+    sqlQuery = sqlQuery + ' AND cate_descrip = "' + req.query.category + '"';
+  }
+  if (req.query.publisher) {
+    sqlQuery = sqlQuery + ' AND pub_name = "' + req.query.publisher + '"';
+  }
+  if (req.query.plt) {
+    sqlQuery = sqlQuery + ' AND book_price < "' + req.query.plt + '"';
+  }
+  if (req.query.pgt) {
+    sqlQuery = sqlQuery + ' AND book_price > "' + req.query.pgt + '"';
+  }
+  sqlQuery = sqlQuery + ';';
   conn.query(sqlQuery, (err, data) => {
     if (err) {
       console.log(err.message);
@@ -49,7 +47,6 @@ app.get('/booksfulldata', (req, res) => {
   });
 });
 
-// SERVER START
 app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}`);
 }); 
