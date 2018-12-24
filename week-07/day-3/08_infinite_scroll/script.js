b1cd1b43d33165fee30952a20db293f503264aa5
Andras Gerencser <andras.gerencser@gmail.com>
2018-12-24 17:18:36 +0100

    feat(Infinite scroll) add scroll up capability

--- week-07/day-3/08_infinite_scroll/script.js
+++ week-07/day-3/08_infinite_scroll/script.js
@@ -1,21 +1,30 @@
 const body = document.body;
 let scrollThreshold = 300;
 
-let createTenBoxes = () => {
+let createTenBoxes = (position) => {
   for (let i = 1; i <= 10; i ++) {
     const square = document.createElement('div');
     let r = Math.floor(Math.random() * 256);
     let g = Math.floor(Math.random() * 256);
     let b = Math.floor(Math.random() * 256);
     square.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-    body.appendChild(square);
+    if (position === 'after') {
+      body.appendChild(square);
+    }
+    if (position === 'before') {
+      body.insertBefore(square, body.childNodes[0]);
+    }
   }
 }
 
-createTenBoxes();
+createTenBoxes('after');
 
 window.addEventListener('scroll', event => {
   if (window.scrollY + window.innerHeight >= body.offsetHeight - scrollThreshold) {
-    createTenBoxes();
+    createTenBoxes('after');
+  }
+  console.log(window.scrollY);
+  if (window.scrollY < scrollThreshold) {
+    createTenBoxes('before');
   }
 });
