3038de9813e3c75207c274705ab825fc0b446f3d
Andras Gerencser <andras.gerencser@gmail.com>
2018-12-25 12:14:39 +0100

    feat(greenfox-logo) add js

---
+++ week-06/day-2/12_greenfox_logo/greenfox-logo.js
@@ -0,0 +1,8 @@
+// Select left and right eyes
+// They have #left-eye and #right-eye
+// turn their color (fill) to tomato
+
+const leftEye = document.querySelector('#left-eye');
+const rightEye = document.querySelector('#right-eye');
+leftEye.setAttribute('fill', 'tomato');
+rightEye.setAttribute('fill', 'tomato');
