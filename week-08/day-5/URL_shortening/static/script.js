fed72b7c5fa8e32d70613dd26ee303311ebb5a7a
Andras Gerencser <andras.gerencser@gmail.com>
2019-01-03 10:23:32 +0100

    feat(URL shortening) add status 400 handling to end point

--- week-08/day-5/URL_shortening/static/script.js
+++ week-08/day-5/URL_shortening/static/script.js
@@ -1,5 +1,6 @@
 const form = document.querySelector('form');
 const { url, alias } = form.elements;
+const h1 = document.querySelector('h1');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   postXhr = new XMLHttpRequest();
@@ -10,9 +11,15 @@ form.addEventListener('submit', (event) => {
     alias: alias.value
   }));
   postXhr.onload = () => {
-    //todo: lekezelni a 400-as kódot
-    const record = JSON.parse(postXhr.responseText);
-    const h1 = document.querySelector('h1');
-    h1.textContent = `Your URL is aliased to ${record.alias} and your secret code is ${record.secretCode}.`;
+    if (postXhr.status === 400) {
+      h1.style.color = 'red';
+      h1.textContent = `Your alias is already in use!`;
+    }
+    if (postXhr.status === 200) {
+      const record = JSON.parse(postXhr.responseText);
+      h1.style.color = 'black';
+      h1.textContent = `Your URL is aliased to ${record.alias} and your secret code is ${record.secretCode}.`;
+      form.reset();
+    }
   }
 });
