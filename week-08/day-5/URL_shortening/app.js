b8f43fd1cccd9de755454696989e270146660ead
Andras Gerencser <andras.gerencser@gmail.com>
2019-01-03 13:25:30 +0100

    feat(URL shortening) add /api/links endpoint

--- week-08/day-5/URL_shortening/app.js
+++ week-08/day-5/URL_shortening/app.js
@@ -41,6 +41,38 @@ app.post('/api/links', (req, res) => {
   });
 });
 
+
+app.get('/a/:alias', (req, res) => {
+  const sqlQueryHitCount = `UPDATE urls SET hitCount = hitCount + 1 WHERE alias = ?;`;
+  conn.query(sqlQueryHitCount, [req.params.alias], (err, data) => {
+    if (err) {
+      res.status(404);
+    } else {
+      console.log(data); //csak egy OKPacketet kapok vissza, még ki kell szedni az url-t
+      res.status(200);
+      // If the alias exists it should increment the hit count and redirect to the URL 
+    }
+  });
+});
+
+app.get('/api/links', (req, res) => {
+  const sqlQuery = 'SELECT id, url, alias, hitCount FROM urls;';
+  conn.query(sqlQuery, (err, rows) => {
+    if (err) {
+      res.status(400);
+    } else {
+      res.json(rows);
+    }
+  });
+});
+
+app.delete('/api/links/:id', (req, res) => {
+// The secret code should be in the request's body in JSON format
+// If it doesn't exists respond with 404 status code
+// If it exists but the provided secret code doesn't match respond with 403 status code
+// If it exists and the provided secret code matches delete the entry from the database and respond with 204 status code
+});
+
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}`);
 });
