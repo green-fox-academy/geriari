50a5fa689762f937985ac1c4530170921ec7b26d
Andras Gerencser <andras.gerencser@gmail.com>
2019-01-08 12:24:08 +0100

    feat(quiz) add app.js

---
+++ week-09/day-4/quiz/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const path = require('path');
+const PORT = 3000;
+
+app.use(express.json());
+
+const conn = mysql.createConnection({
+  user: 'root',
+  password: 'password',
+  database: 'quiz',
+  port: 3306
+});
+
+app.use('/static', express.static('static'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}`);
+});
